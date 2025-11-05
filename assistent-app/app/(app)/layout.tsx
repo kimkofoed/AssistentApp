3ecfeb1c60729e271c6ae8688636af449371715a
Kim Sørensen <kimsorensen@MacBookPro.home>
2025-11-05 21:51:42 +0100

    implicitly has an 'any' type

--- assistent-app/app/(app)/layout.tsx
+++ assistent-app/app/(app)/layout.tsx
@@ -4,15 +4,19 @@ import Sidebar from "@/components/Sidebar";
 import Topbar from "@/components/Topbar";
 import MobileNav from "@/components/MobileNav";
 
-export default function AppShell({ children }) {
+interface AppShellProps {
+  children: React.ReactNode;
+}
+
+export default function AppShell({ children }: AppShellProps) {
   return (
     <div className="flex min-h-screen">
-    <Sidebar />
-    <div className="flex-1 flex flex-col">
+      <Sidebar />
+      <div className="flex-1 flex flex-col">
         <Topbar />
         <main className="p-6 max-w-6xl mx-auto w-full">{children}</main>
         <MobileNav />
-    </div>
+      </div>
     </div>
   );
 }
